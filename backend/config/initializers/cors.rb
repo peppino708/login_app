13215ed1b6f9d308ff03c61d2b94e4f922abb66b
github peppino708 <708.baseball.ster@gmail.com>
2022-03-11 20:39:19 +0900

    Change comment

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001" # React側はポート番号3000で作るので「localhost:3000」を指定
+    origins "localhost:3001" # React側はポート番号3001で作るので「localhost:3000」を指定
 
     resource "*",
       headers: :any,
